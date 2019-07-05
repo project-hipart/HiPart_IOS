1ceacee3f348209efeba7ab701a6f4160e704d4f
문명주 <eun95828@likelion.org>
2019-07-06 08:13:15 +0900

    add filter change ViewController

---
+++ Resources/Font.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIFont{
+	public static let nanumRegular = UIFont(name:"NanumSquareRegular",size: 12)!
+	public static let nanumBold = UIFont(name:"NanumSquareBold",size: 12)!
+	public static let nanumExtraBold = UIFont(name:"NanumSquareExtraBold",size: 12)!
+	public static let nanumLight = UIFont(name:"NanumSquareLight",size: 12)!
+	
+}
